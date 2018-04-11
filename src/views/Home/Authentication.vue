1ae652bf656d7c351a5df8c3a09770822c6a9080
zys8119 <770959294@qq.com>
2018-04-11 10:39:58 +0800

    旧公司信息不全兼容处理

--- src/views/Home/Authentication.vue
+++ src/views/Home/Authentication.vue
@@ -80,82 +80,82 @@
             ...mapActions(['action']),
             homeSubmitNext(){
                 if(!this.airforce.homeSubmit.auth_name || this.airforce.homeSubmit.auth_name.length == 0){
-                    this.$vux.toast.text("【姓名】<br>不能为空");
+                    this.$vux.toast.text("【姓名】<br>不能为空,请补全公司信息");
                     return
                 }
                 if(!/^[\u4E00-\u9FA5]{2,}$/.test(this.airforce.homeSubmit.auth_name)){
-                    this.$vux.toast.text("【姓名】<br>至少2个中文字符");
+                    this.$vux.toast.text("【姓名】<br>至少2个中文字符,请补全公司信息");
                     return
                 }
                 if(!this.airforce.homeSubmit.auth_phone ||
                     this.airforce.homeSubmit.auth_phone.length == 0){
-                    this.$vux.toast.text("【手机号码】<br>不能为空");
+                    this.$vux.toast.text("【手机号码】<br>不能为空,请补全公司信息");
                     return
                 }
                 if(!utils.isPhone(this.airforce.homeSubmit.auth_phone)){
-                    this.$vux.toast.text("请输入正确的<br>【手机号码】");
+                    this.$vux.toast.text("请输入正确的<br>【手机号码】,请补全公司信息");
                     return
                 }
                 if(!this.airforce.homeSubmit.auth_linkman1 ||
                     this.airforce.homeSubmit.auth_linkman1.length == 0){
-                    this.$vux.toast.text("【紧急联系人】<br>不能为空");
+                    this.$vux.toast.text("【紧急联系人】<br>不能为空,请补全公司信息");
                     return
                 }
                 if(!/^[\u4E00-\u9FA5]{2,}$/.test(this.airforce.homeSubmit.auth_linkman1)){
-                    this.$vux.toast.text("【紧急联系人】<br>至少2个中文字符");
+                    this.$vux.toast.text("【紧急联系人】<br>至少2个中文字符,请补全公司信息");
                     return
                 }
                 if(!this.airforce.homeSubmit.auth_linkman1_Select){
-                    this.$vux.toast.text("请选择【紧急联系人1】关系");
+                    this.$vux.toast.text("请选择【紧急联系人1】关系,请补全公司信息");
                     return
                 }
                 if(!this.airforce.homeSubmit.auth_linkman1_phone ||
                     this.airforce.homeSubmit.auth_linkman1_phone.length == 0){
-                    this.$vux.toast.text("【联系人1手机】<br>不能为空");
+                    this.$vux.toast.text("【联系人1手机】<br>不能为空,请补全公司信息");
                     return
                 }
                 if(!utils.isPhone(this.airforce.homeSubmit.auth_linkman1_phone)){
-                    this.$vux.toast.text("请输入正确的<br>【联系人1手机】");
+                    this.$vux.toast.text("请输入正确的<br>【联系人1手机】,请补全公司信息");
                     return
                 }
                 if(!this.airforce.homeSubmit.auth_linkman2 ||
                     this.airforce.homeSubmit.auth_linkman2.length == 0){
-                    this.$vux.toast.text("【紧急联系人2】<br>不能为空");
+                    this.$vux.toast.text("【紧急联系人2】<br>不能为空,请补全公司信息");
                     return
                 }
                 if(!/^[\u4E00-\u9FA5]{2,}$/.test(this.airforce.homeSubmit.auth_linkman2)){
-                    this.$vux.toast.text("【紧急联系人2】<br>至少2个中文字符");
+                    this.$vux.toast.text("【紧急联系人2】<br>至少2个中文字符,请补全公司信息");
                     return
                 }
                 if(!this.airforce.homeSubmit.auth_linkman2_Select){
-                    this.$vux.toast.text("请选择【紧急联系人2】关系");
+                    this.$vux.toast.text("请选择【紧急联系人2】关系,请补全公司信息");
                     return
                 }
                 if(!this.airforce.homeSubmit.auth_linkman2_phone ||
                     this.airforce.homeSubmit.auth_linkman2_phone.length == 0){
-                    this.$vux.toast.text("【联系人2手机】<br>不能为空");
+                    this.$vux.toast.text("【联系人2手机】<br>不能为空,请补全公司信息");
                     return
                 }
                 if(!utils.isPhone(this.airforce.homeSubmit.auth_linkman2_phone)){
-                    this.$vux.toast.text("请输入正确的<br>【联系人2手机】");
+                    this.$vux.toast.text("请输入正确的<br>【联系人2手机】,请补全公司信息");
                     return
                 }
                 if(!this.airforce.homeSubmit.auth_bankCode ||
                     this.airforce.homeSubmit.auth_bankCode.length == 0){
-                    this.$vux.toast.text("【银行卡卡号】<br>不能为空");
+                    this.$vux.toast.text("【银行卡卡号】<br>不能为空,请补全公司信息");
                     return
                 }
                 if(!/^(\d{16}|\d{19})$/.test(this.airforce.homeSubmit.auth_bankCode)){
-                    this.$vux.toast.text("请输入正确的<br>【银行卡卡号】");
+                    this.$vux.toast.text("请输入正确的<br>【银行卡卡号】,请补全公司信息");
                     return
                 }
                 if(!this.airforce.homeSubmit.auth_idCode ||
                     this.airforce.homeSubmit.auth_idCode.length == 0){
-                    this.$vux.toast.text("【身份证号】<br>不能为空");
+                    this.$vux.toast.text("【身份证号】<br>不能为空,请补全公司信息");
                     return
                 }
                 if(!utils.isIdCard(this.airforce.homeSubmit.auth_idCode)){
-                    this.$vux.toast.text("请输入正确的<br>【身份证号】");
+                    this.$vux.toast.text("请输入正确的<br>【身份证号】,请补全公司信息");
                     return
                 }
                 let cartype = 1;
@@ -239,19 +239,27 @@
                         this.airforce.change.set(res.data.name,'auth_name','homeSubmit');
                         this.airforce.change.set(res.data.phone,'auth_phone','homeSubmit');
 
-                        let  role = _.find(this.selectData,{name:res.data.role})
-                        this.airforce.change.set(res.data.role_name,'auth_linkman1','homeSubmit');
-                        this.airforce.change.set(role.name,'auth_linkman1_SelectTxt','homeSubmit');
-                        this.airforce.change.set([role.value],'auth_linkman1_SelectVal','homeSubmit');
-                        this.airforce.change.set(role,'auth_linkman1_Select','homeSubmit');
-                        this.airforce.change.set(res.data.jjphone,'auth_linkman1_phone','homeSubmit');
 
-                        let  role2 = _.find(this.selectData2,{name:res.data.role_2});
+                        this.airforce.change.set(res.data.role_name,'auth_linkman1','homeSubmit');
+                        this.airforce.change.set(res.data.jjphone,'auth_linkman1_phone','homeSubmit');
+                        try {
+                            let  role = _.find(this.selectData,{name:res.data.role})
+                            this.airforce.change.set(role.name,'auth_linkman1_SelectTxt','homeSubmit');
+                            this.airforce.change.set([role.value],'auth_linkman1_SelectVal','homeSubmit');
+                            this.airforce.change.set(role,'auth_linkman1_Select','homeSubmit');
+                        }catch (e){}
+
+
+
+
                         this.airforce.change.set(res.data.role_2_name,'auth_linkman2','homeSubmit');
-                        this.airforce.change.set(role2.name,'auth_linkman2_SelectTxt','homeSubmit');
-                        this.airforce.change.set([role2.value],'auth_linkman2_SelectVal','homeSubmit');
-                        this.airforce.change.set(role2,'auth_linkman2_Select','homeSubmit');
                         this.airforce.change.set(res.data.jjphone_2,'auth_linkman2_phone','homeSubmit');
+                        try {
+                            let role2 = _.find(this.selectData2, {name: res.data.role_2});
+                            this.airforce.change.set(role2.name, 'auth_linkman2_SelectTxt', 'homeSubmit');
+                            this.airforce.change.set([role2.value], 'auth_linkman2_SelectVal', 'homeSubmit');
+                            this.airforce.change.set(role2, 'auth_linkman2_Select', 'homeSubmit');
+                        }catch (e){}
 
                         this.airforce.change.set(res.data.idcard,'auth_idCode','homeSubmit');
                         this.airforce.change.set(res.data.cardno,'auth_bankCode','homeSubmit');
